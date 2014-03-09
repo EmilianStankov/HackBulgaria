876dc4f210bc7f30ded292741a1390e17c7c49e7
EmilianStankov <eminstine@abv.bg>
2014-03-09 23:38:11 +0200

    Delete oned_robot.py

--- week0/bonus round 3 - one dimensional robot/oned_robot.py
+++
@@ -1,9 +0,0 @@
-def final_position(commands, a, b):
-	position = 0
-	for char in commands:
-		if char == 'R' and position < b:
-			position += 1
-		elif char == 'L' and position > -a:
-			position -= 1
-
-	return position
